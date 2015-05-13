a0068ca9d0d655f5b3b9e18776714958be8b439b
magnolo <office@manfredwalder.at>
2015-05-13 08:48:01 +0200

    change

--- app/scripts/controllers/articles-article.js
+++ app/scripts/controllers/articles-article.js
@@ -13,4 +13,15 @@ angular.module('minovateApp')
       title: 'Der Arikeltext',
       subtitle: 'Artikel'
     };
+    $scope.article = {
+      title: 'Titelchen',
+      intro: '',
+      text: '',
+      is_online: 1,
+      online_at: '',
+      created_at: '2015-05-05 15:12:00',
+      updated_at: '2015-05-05 15:12:00',
+      image_id: 0,
+      image: {}
+    };
   });
